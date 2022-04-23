30f6bb6a6c4f799d16de691a2a2fd1839b257f91
Ericlys Moreira <ericlysm@gmail.com>
2022-04-22 23:46:59 -0300

    feat: componente header

---
+++ src/components/Header.tsx
@@ -0,0 +1,46 @@
+import { Box, Flex, Icon, Link as ChakraLink, SimpleGrid } from "@chakra-ui/react";
+import { Image } from '@chakra-ui/react'
+import Link from "next/link";
+
+import { RiArrowLeftSLine } from 'react-icons/ri';
+
+interface HeaderProps {
+  backHref?: string;
+}
+
+export function Header({ backHref }: HeaderProps){
+  return(
+    <Flex
+      as="header"
+      w="100%"
+      maxWidth={1440}
+      h="24"
+      mx="auto"
+      px="6"
+    >
+      <SimpleGrid flex="1" columns={3}>
+      <Box
+        display="flex"
+        alignItems="center"
+        ml={[ 0, 0, 0,"172"]}
+      >
+      {!!backHref && (
+        <Link href={backHref} passHref>
+          <ChakraLink display="flex" alignItems="center">
+            <Icon as={RiArrowLeftSLine} fontSize="32" />
+          </ChakraLink>
+        </Link>
+      )}
+      </Box>
+      <Box
+       justifyItems={"center"}
+       display="flex"
+       alignItems="center"
+       justifyContent="center"
+      >
+        <Image w={184} h="auto"src="/images/logo.svg" alt="logo"/>
+      </Box>
+      </SimpleGrid>
+    </Flex>
+  )
+}
