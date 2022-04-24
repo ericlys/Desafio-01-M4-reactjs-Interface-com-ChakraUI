d5881c7dedb367de535e88c9008f50666ddf12fc
Ericlys Moreira <ericlysm@gmail.com>
2022-04-23 23:13:20 -0300

    feat: divider e texto de titulo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Divider, Flex, Text } from "@chakra-ui/react";
 import { Banner } from "../components/Banner";
 import { Header } from "../components/Header";
 import { TravelType } from "../components/TravelType";
@@ -10,12 +10,18 @@ export default function Home() {
       <Box h={368.21}>
         <Banner />
       </Box>
+
       <Flex
-       w={1160}
-       h={145}
-       mt={80.74}
-       mx="auto"
-       justifyContent="space-between"
+        flexDirection="column"
+        w="100%"
+        maxWidth={1160}
+        mx="auto"
+      >
+      <Flex
+        flex="1"
+        h={145}
+        mt={80.74}
+        justifyContent="space-between"
        >
         <TravelType srcImage="/images/cocktail.svg" text="Vida noturna" />
         <TravelType srcImage="/images/surf.svg" text="praia" />
@@ -23,6 +29,24 @@ export default function Home() {
         <TravelType srcImage="/images/museum.svg" text="clássico" />
         <TravelType srcImage="/images/earth.svg" text="e mais..." />
       </Flex>
+
+      <Divider 
+        borderColor="gray.600" 
+        border={2}
+        w={90}
+        mx="auto"
+        mt="20"
+        mb="3.25rem"
+      />
+
+      <Text textAlign="center" fontSize="4xl" fontWeight={500}>
+        Vamos nessa?<br/>Então escolha seu continente
+      </Text>
+
+
+
+      </Flex>
+
     </Flex>
   ) 
 }
