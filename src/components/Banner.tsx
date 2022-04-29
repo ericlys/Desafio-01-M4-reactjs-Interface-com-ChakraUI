5ebe4f2f3e50b8828c5f0a41d6f2fe0f09a6a649
Ericlys Moreira <ericlysm@gmail.com>
2022-04-29 01:05:18 -0300

    feat: adicionando responsividade no banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,56 +1,95 @@
-import { Box, Image, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Image, SimpleGrid, Text, useBreakpointValue } from "@chakra-ui/react";
 
 interface BannerProps {
   amountContinents?: number;
 }
 
 export function Banner( { amountContinents = 5 }: BannerProps){
+
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    md: true,
+  })
+
   return(
     <Box 
-      h={335}
+      h={[163,335]}
       bgImage="https://images.unsplash.com/photo-1495344517868-8ebaf0a2044a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=653&q=80"
       bgRepeat="no-repeat"
-      bgSize="cover"
-      bgPos="0% 24.4%"
+      bgSize={["auto", "cover"]}
+      bgPos={[ "0% 34%", "0% 5%","0% 24.4%"]}
       >
-     <SimpleGrid h={368.21} columns={2}>
-        <Box 
-          display="flex"
-          justifyContent="center"
-          flexDir="column"
-          px="2"
-          mx="auto"
-        >
-          <Text 
-            color="white" 
-            fontSize="4xl"
-            fontWeight="500" 
-            maxWidth={426}
+        {!isWideVersion && (
+          <Box 
+            display="flex"
+            justifyContent="center"
+            flexDir="column"
+            h="100%"
+            px="4"
+            mx="auto"
           >
-            {amountContinents} Continentes,<br/> infinitas possibilidades.
-          </Text>
-          <Text 
-            mt={"20px"}
-            fontSize="20px"
-            color="light.500"
-            maxWidth={525}
-          >
-            Chegou a hora de tirar do papel a viagem que você sempre sonhou. 
-          </Text>
-        </Box>
-        <Box 
-          display="flex"
-          justifyContent="end"
-          alignItems="end"
-          mx="auto"
-        >
-          <Image
-           h={270}
-           src="images/airplane.svg" 
-           alt="airplane"
-           />
-        </Box>
-     </SimpleGrid>
+            <Text 
+              color="white" 
+              fontSize="xl"
+              fontWeight="500" 
+              maxWidth={238}
+            >
+              {amountContinents} Continentes,<br/> infinitas possibilidades.
+            </Text>
+            <Text 
+              mt={2}
+              fontSize="sm"
+              color="light.500"
+              maxWidth={525}
+            >
+              Chegou a hora de tirar do papel a viagem que você sempre sonhou. 
+            </Text>
+          </Box>
+        )}
+
+      { isWideVersion && (
+            <SimpleGrid h={368.21} columns={2}>
+              <Box 
+                display="flex"
+                justifyContent="center"
+                flexDir="column"
+                px="2"
+                mx="auto"
+              >
+                <Text 
+                  color="white" 
+                  fontSize="4xl"
+                  fontWeight="500" 
+                  maxWidth={426}
+                >
+                  {amountContinents} Continentes,<br/> infinitas possibilidades.
+                </Text>
+                <Text 
+                  mt={"20px"}
+                  fontSize="20px"
+                  color="light.500"
+                  maxWidth={525}
+                >
+                  Chegou a hora de tirar do papel a viagem que você sempre sonhou. 
+                </Text>
+              </Box>
+              <Box 
+                display="flex"
+                justifyContent="end"
+                alignItems="end"
+                mx="auto"
+              >
+                <Image
+                h={270}
+                src="images/airplane.svg" 
+                alt="airplane"
+                />
+              </Box>
+            </SimpleGrid>
+          )
+      }
+
+
     </Box>
   )
 }
